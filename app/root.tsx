85e7d70853f2913ce4c7b02fa1ce5c4b7f3a9ec3
Ayush Gupta <guptaayush2526@gmail.com>
2026-03-27 22:24:24 +0530

    added puterjs and authentication

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,11 @@ import {
 
 import type { Route } from "./+types/root";
 import "./app.css";
+import { useEffect, useState } from "react";
+import { getCurrentUser,
+  signIn as puterSignIn,
+  signOut as puterSignOut,
+ } from "lib/puter.action"
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -41,9 +46,58 @@ export function Layout({ children }: { children: React.ReactNode }) {
   );
 }
 
-export default function App() {
-  return <Outlet />;
+const DEFAULT_AUTH_STATE: AuthState ={
+  isSignedIn: false,
+  username: null,
+  userId: null,
 }
+
+export default function App() {
+  const [authState, setAuthState] = useState<AuthState>(DEFAULT_AUTH_STATE);
+
+  const refreshAuth = async () => {
+    try {
+      const user = await getCurrentUser();
+        setAuthState({
+          isSignedIn: !! user,
+          username: user?.username || null,
+          userId: user?.uuid || null,
+        });
+        
+    } catch {
+      setAuthState(DEFAULT_AUTH_STATE);
+      return false;
+    }
+  };
+
+  useEffect(() => {
+    refreshAuth();
+  }, []);
+
+  const signIn = async () => {
+    await puterSignIn();
+    return await refreshAuth();
+  }
+
+  const signOut = async () => {
+    await puterSignOut();
+    return await refreshAuth();
+  }
+
+  return ( 
+    <main className="min-h-screen bg-background text-foreground relative z-10">
+      <Outlet 
+        context={{
+          ...authState,
+          refreshAuth,
+          signIn,
+          signOut,
+        }}  
+    />
+    </main>
+  );
+}
+
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
   let message = "Oops!";
