cdfee1470a803c043e6443ec823c127e25acbd5f
Ayush Gupta <guptaayush2526@gmail.com>
2026-04-05 23:29:22 +0530

    small update

--- app/routes/visualizer.$id.tsx
+++ app/routes/visualizer.$id.tsx
@@ -1,8 +1,49 @@
-import { useLocation } from "react-router"
+import { useLocation, useNavigate } from "react-router"
+import { useEffect, useRef, useState } from "react";
+import { generate3DView } from "lib/ai.action";
+import
 
 const visualizer = () => {
+  const navigate = useNavigate();
   const location = useLocation();
   const { initialImage, name } = location.state || {};
+
+  const hasInitialGenerated = useRef(false);
+
+  const [isProcessing, setIsProcessing] = useState(false);
+  const [currentImage, setCurrentImage] = useState<string | null>(null);
+
+  const handleBack = () => {
+    if(!initialImage) return;
+
+    try{
+      setIsProcessing(true);
+      const result = await generate3DView({ sourceImage: initialImage });
+
+      if(result.renderedImage){
+      setCurrentImage(result.renderedImage);
+
+      // update the project with the new rendered image
+
+    }
+  } catch (error) {
+      console.error("Error generating 3D view:", error);
+  } finally {
+      setIsProcessing(false);
+  }
+}
+
+  useEffect(() => {
+    if(initialImage || hasInitialGenerated.current) return;
+
+    if(!initialRender){
+      setCurrentImage(initialRender);
+      hasInitialGenerated.current = true;
+      return;
+    }
+    hasInitialGenerated.current = true;
+    runGeneration();
+  }, [initialImage, hasInitialGenerated]);
 
   return (
     <section>
